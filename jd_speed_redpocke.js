22c30ed7c90e419fe11f5ac73ed0f25e8ab76beb
slj844909754 <57609360+slj844909754@users.noreply.github.com>
2021-02-22 16:42:28 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -162,7 +162,7 @@ function invite() {
     body: dataString
   };
   $.post(options, (err, resp, data) => {
-    console.log(`您的好友助力码为：${inviterPin}`)
+    console.log(`您的好友助力码为：${data.inviterPin}`)
   })
 }
 
